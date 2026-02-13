8ff8308b6e3d3f374cf87cdb468dded47eff6512
RISHIKUMAR206 <rishikumarjha005@gmail.com>
2026-02-13 03:28:42 -0300

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+About Task:👤🔐 This shell script demonstrates Linux user and group management.
+It creates a user, sets a password, creates a group, adds the user to that group, and displays user details.
+It is created to practice basic Linux system administration commands. 🖥️⚙️
 
 # Name   : Rishi Kumar Jha
 # Email  :rishikumarjha005@gmail.com
 # Course : Cloud & DevOps
+
+
+#!/bin/bash
 
 echo "Create user"
 sudo useradd rishi
